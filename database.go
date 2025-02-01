76d6f2efc0b7b1be14d38f8711ca91fffa1830d5
Vladimir <myweb11@mail.ru>
2025-02-01 12:01:02 +0300

    fix tests, прием sql запроса из командной строки

---
+++ database.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"runtime"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type ExecuteResult struct {
+	Success bool   `json:"success"`
+	Data    string `json:"data"`
+	Error   string `json:"error"`
+	Time    string `json:"time"`
+	Memory  string `json:"memory"`
+}
+
+func Execute(sql string) string {
+    start := time.Now()
+
+	// Чтение .env файла
+	godotenv.Load(".env")
+
+	// Лексический анализатор
+	lexer := NewLexer(sql)
+
+	// Токены лексического анализа
+	tokens, err := lexer.Analyze()
+
+	if err != nil {
+		result, _ := json.Marshal(
+			ExecuteResult{
+				Success: false,
+				Error:   err.Error(),
+			})
+		return string(result)
+	}
+
+	// Парсер
+	parser := NewParser(tokens)
+
+	// Подготовленный запрос
+	sqlQuery, err := parser.parse()
+
+	if err != nil {
+		result, _ := json.Marshal(
+			ExecuteResult{
+				Success: false,
+				Error:   err.Error(),
+			})
+		return string(result)
+	}
+
+	// Executor
+	executor := NewExecutor(sqlQuery)
+
+	// Выполняем запрос
+	data, err := executor.executeQuery()
+
+	if err != nil {
+		result, _ := json.Marshal(
+			ExecuteResult{
+				Success: false,
+				Error:   err.Error(),
+			})
+
+		return string(result)
+	}
+
+	duration := time.Since(start)
+
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	result, _ := json.Marshal(
+		ExecuteResult{
+			Success: true,
+			Data:    data,
+			Error:   "",
+			Time:    duration.String(),
+			Memory:  humanReadableBytes(memStats.Alloc),
+		})
+
+	return string(result)
+}
+
+func ErrorArgs() string {
+    result, _ := json.Marshal(
+        ExecuteResult{
+            Success: false,
+            Error:   "Укажите один параметр в качестве sql запроса",
+        })
+
+    return string(result)
+}
--- main.go
+++ main.go
@@ -1,104 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"runtime"
-	"time"
-
-	"github.com/joho/godotenv"
+	"os"
 )
 
-type ExecuteResult struct {
-	Success bool   `json:"success"`
-	Data    string `json:"data"`
-	Error   string `json:"error"`
-	Time    string `json:"time"`
-	Memory  string `json:"memory"`
-}
-
 func main() {
-	sql := `
-	CREATE TABLE Customers (
-		id int AUTO_INCREMENT,
-		CustomerName text NULL,
-		ContactName text,
-		Address text not null,
-		City text,
-		Country text,
-		date date
-	);
-	`
-
-	start := time.Now()
-
-	// Чтение .env файла
-	godotenv.Load(".env")
-
-	// Лексический анализатор
-	lexer := NewLexer(sql)
-
-	// Токены лексического анализа
-	tokens, err := lexer.Analyze()
-
-	if err != nil {
-		result, _ := json.Marshal(
-			ExecuteResult{
-				Success: false,
-				Error:   err.Error(),
-			})
-		fmt.Println(string(result))
-
+	if len(os.Args) != 2 {
+		fmt.Println(ErrorArgs())
 		return
 	}
 
-	// Парсер
-	parser := NewParser(tokens)
+	sql := os.Args[1]
 
-	// Подготовленный запрос
-	sqlQuery, err := parser.parse()
-
-	if err != nil {
-		result, _ := json.Marshal(
-			ExecuteResult{
-				Success: false,
-				Error:   err.Error(),
-			})
-		fmt.Println(string(result))
-
-		return
-	}
-
-	// Executor
-	executor := NewExecutor(sqlQuery)
-
-	// Выполняем запрос
-	data, err := executor.executeQuery()
-
-	if err != nil {
-		result, _ := json.Marshal(
-			ExecuteResult{
-				Success: false,
-				Error:   err.Error(),
-			})
-		fmt.Println(string(result))
-
-		return
-	}
-
-	duration := time.Since(start)
-
-	var memStats runtime.MemStats
-	runtime.ReadMemStats(&memStats)
-
-	result, _ := json.Marshal(
-		ExecuteResult{
-			Success: true,
-			Data:    data,
-			Error:   "",
-			Time:    duration.String(),
-			Memory:  humanReadableBytes(memStats.Alloc),
-		})
-
-	fmt.Println(string(result))
+	fmt.Println(Execute(sql))
 }
